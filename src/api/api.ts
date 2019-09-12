8e3052094efcf8f58fee00e754dbce20a5c1d23d
Paweł Fajfer <pawel.fajfer@hltech.com>
2019-09-12 14:05:56 +0200

    Changed nginx config - added proxy.

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,10 +2,8 @@ import axios, { AxiosResponse } from 'axios';
 import { Environment } from '../components/types';
 import { mapServiceDtoToService } from '../utils/helpers/MappingHelpers';
 import { EnvironmentStateDto, ServiceDto } from './api.types';
-import { config } from './config';
 
-const baseURL = config.get('BASE_PATH');
-const axiosWithBaseUrl = axios.create({ baseURL });
+const axiosWithBaseUrl = axios.create({ baseURL: '/api' });
 
 export function getEnvironmentServicesRequest(env: Environment) {
     return axiosWithBaseUrl
