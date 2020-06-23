bedc68cd62bedddb21a38934ef65ac0b8c87c566
Karol Maciolek <karol.maciolek@hltech.com>
2020-06-23 18:43:37 +0200

    Adds demo via Docker section to README Adds docker-compose file that starts ui with backend and db Modifies demoGenerator.sh script to accept custom backend url

--- demo/demoGenerator.sh
+++ demo/demoGenerator.sh
@@ -1,5 +1,9 @@
 #!usr/bin/env sh
 
+URL=${1:-localhost:8080}
+
+echo "$URL"
+
 # Updating environments
 for envFilename in $(ls environments/)
 do
@@ -7,7 +11,7 @@ do
     echo "Updating environment: $env"
 	
 	#updating environment
-	curl --request PUT -H "Content-Type: application/json" -d @environments/$envFilename -s -w "Response status: %{http_code}\n"  http://judge-d.herokuapp.com/environments/$env -o /dev/null
+	curl --request PUT -H "Content-Type: application/json" -d @environments/$envFilename -s -w "Response status: %{http_code}\n"  http://$URL/environments/$env -o /dev/null
 done 
 
 #Updating contracts
@@ -19,5 +23,5 @@ do
     echo "Updating contracts: service name - $name service version - $version"
 	
 	#update contracts
-	curl --request POST -H "Content-Type: application/json" -d @contracts/$conFilename -s -w "Response status: %{http_code}\n" http://judge-d.herokuapp.com/contracts/services/$name/versions/$version -o /dev/null
+	curl --request POST -H "Content-Type: application/json" -d @contracts/$conFilename -s -w "Response status: %{http_code}\n" http://$URL/contracts/services/$name/versions/$version -o /dev/null
 done 
